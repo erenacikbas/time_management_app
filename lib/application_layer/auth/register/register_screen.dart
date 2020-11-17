101bb70072c2c0d6d8421d6f21764e19340340f1
Eren Tuna Açıkbaş <erentuna@Eren-MacBook-Pro.local>
2020-11-17 12:31:58 +0300

    small updates

--- lib/application_layer/auth/register/register_screen.dart
+++ lib/application_layer/auth/register/register_screen.dart
@@ -26,85 +26,86 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return loading
         ? LoadingScreen()
         : Scaffold(
-          backgroundColor: Colors.brown[100],
-          appBar: AppBar(
-            backgroundColor: Colors.brown[400],
-            elevation: 0.0,
-            title: Text("Sign up to Brew Crew"),
-            actions: <Widget>[
-              FlatButton.icon(
-                icon: Icon(Icons.person),
-                label: Text("Sign in"),
-                onPressed: () => widget.toogleView(),
-              ),
-            ],
-          ),
-          body: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration:
-                        textInputDecoration.copyWith(hintText: "Email"),
-                    validator: (val) =>
-                        val.isEmpty ? "Enter an email" : null,
-                    onChanged: (val) {
-                      setState(() => email = val);
-                    },
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    decoration:
-                        textInputDecoration.copyWith(hintText: "Password"),
-                    validator: (val) => val.length < 6
-                        ? "Enter a password 6+ chars long"
-                        : null,
-                    onChanged: (val) {
-                      setState(() => password = val);
-                    },
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  RaisedButton(
-                    color: Colors.pink[400],
-                    child: Text(
-                      "RegisterScreen",
-                      style: TextStyle(color: Colors.white),
+            appBar: AppBar(
+              backgroundColor: Colors.indigoAccent,
+              elevation: 0.0,
+              title: Text("Sign up to Brew Crew"),
+              // actions: <Widget>[
+              //   FlatButton.icon(
+              //     icon: Icon(Icons.person),
+              //     label: Text("Sign in"),
+              //     onPressed: () => widget.toogleView(),
+              //   ),
+              // ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 20,
                     ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        setState(() => loading = true);
-                        dynamic result = await _auth
-                            .registerWithEmailAndPassword(email, password);
-                        if (result == null) {
-                          setState(() {
-                            error = "please supply a valid email";
-                            loading = false;
-                          });
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "Email"),
+                      validator: (val) => val.isEmpty ? "Enter an email" : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      decoration:
+                          textInputDecoration.copyWith(hintText: "Password"),
+                      validator: (val) => val.length < 6
+                          ? "Enter a password 6+ chars long"
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    RaisedButton(
+                      color: Colors.indigoAccent,
+                      child: Text(
+                        "Register",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() => loading = true);
+                          dynamic result = await _auth
+                              .registerWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = "please supply a valid email";
+                              loading = false;
+                            });
+                          }
                         }
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    height: 12.0,
-                  ),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red),
-                  ),
-                ],
+                      },
+                    ),
+                    SizedBox(
+                      height: 12.0,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    GestureDetector(
+                        onTap: () => widget.toogleView(),
+                        child: Text("Do you have an account ? Sign in"))
+                  ],
+                ),
               ),
             ),
-          ),
-        );
+          );
   }
 }

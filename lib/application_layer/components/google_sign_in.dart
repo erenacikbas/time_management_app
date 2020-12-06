6a5ad9fb73d404c2e4588991590b55d172b937b6
Eren Tuna Açıkbaş <erentuna@Eren-MacBook-Pro.local>
2020-12-06 23:50:28 +0300

    Todo Page improved, Tracker Page Updated, Analyze Page Created,

--- lib/application_layer/components/google_sign_in.dart
+++ lib/application_layer/components/google_sign_in.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:time_management_app/service_layer/database.dart';
 
@@ -32,16 +33,8 @@ Future<String> signInWithGoogle() async {
   var randomizer = new Random();
   var rNum = min + randomizer.nextInt(max - min);
 
-  if (FirebaseFirestore.instance
-      .collection("users")
-      .doc(FirebaseAuth.instance.currentUser.uid)
-      .id
-      .isEmpty) {
-    DatabaseService().updateUserData(currentUser.displayName, currentUser.email,
-        currentUser.photoURL, [], currentUser.uid, rNum, Timestamp.now());
-  } else {
-    print("user has already been created on database");
-  }
+  DatabaseService().updateUserData(currentUser.displayName, currentUser.email,
+      currentUser.photoURL, [], currentUser.uid, rNum, Timestamp.now());
 
   return 'signInWithGoogle succeeded: $user';
 }
